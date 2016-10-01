592f9cc0b385faa694ecacf4dde301254c1dd49a
Aliot <aliot@tut.by>
2016-10-01 11:41:23 +0300

    first controller

--- builds/development/app/app.js
+++ builds/development/app/app.js
@@ -5,12 +5,26 @@ $.material.init();
 
     angular
         .module('ngFit', ['ngRoute'])
-        .config(ngFitConfig);
+        .config(ngFitConfig)
+        .controller('MainCtrl');
 
     function ngFitConfig($routeProvider) {
         $routeProvider
             .when('/',{
-                templateUrl:'/view/index.html'
+                templateUrl:'/view/index.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about',{
+                templateUrl:'/view/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/contact',{
+                templateUrl:'/view/contact.html',
+                controller: 'ContactCtrl'
             });
+    }
+
+    function MainCtrl($scope) {
+        $scope.title = "This is our first scope title"
     }
 })();
