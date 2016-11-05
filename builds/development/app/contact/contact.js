d70a672b4ac2762e658bfc80ab2f310e5485ca20
Aliot <aliot@tut.by>
2016-11-05 14:04:41 +0300

    watch and apply(in comment)

--- builds/development/app/contact/contact.js
+++ builds/development/app/contact/contact.js
@@ -6,14 +6,33 @@
         .config(['$routeProvider', config])
         .controller('ContactCtrl', ContactCtrl);
 
-    ContactCtrl.$inject = ['$scope', '$rootScope', '$log'];
+    ContactCtrl.$inject = ['$scope', '$rootScope', '$log', '$timeout'];
     
-    function ContactCtrl($scope, $rootScope, $log){
+    function ContactCtrl($scope, $rootScope, $log, $timeout){
         var vm = this;
         $rootScope.curPath = 'contact';
         $log.log('contact');
-        $log._first = 'second property';
-        $log.log($log);
+        //$log._first = 'second property';
+        //$log.log($log);
+
+        vm.message = "";
+
+        $scope.$watch('vm.message', function (newVal, oldVal) {
+            console.log('$watch');
+            console.log('newVal', newVal);
+            console.log('oldVal', oldVal);
+        });
+
+        $timeout(function () {
+            vm.message = "Hello!";
+        }, 3000);
+
+        //setTimeout(function () {
+        //    $scope.$apply(function () {
+        //        vm.message = "Hello!";
+        //    })
+        //}, 3000)
+        //  используется apply чтобы обнаруживать изменения появляющиеся благодаря setTimeout
     }
 
     function config($routeProvider) {
